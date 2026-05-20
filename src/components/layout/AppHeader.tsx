2a5e75c93a79c4724df2d1d3ce46de2d64c31fd9
Lucas <lucas.hansson9@gmail.com>
2026-05-20 11:57:56 +0200

    feat: add navigation to app header

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -1,16 +1,39 @@
-import { AppBar, Toolbar, Typography, Button, Stack } from "@mui/material";
+"use client";
+
+import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export default function AppHeader() {
+  const router = useRouter();
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography
+          variant="h6"
+          component="button"
+          onClick={() => router.push("/")}
+          sx={{
+            flexGrow: 1,
+            background: "none",
+            border: "none",
+            color: "inherit",
+            cursor: "pointer",
+            textAlign: "left",
+            font: "inherit",
+          }}
+        >
           Bokningssystem
         </Typography>
 
         <Stack direction="row" spacing={1}>
-          <Button color="inherit">Student</Button>
-          <Button color="inherit">Lärare</Button>
+          <Button color="inherit" onClick={() => router.push("/student")}>
+            Student
+          </Button>
+
+          <Button color="inherit" onClick={() => router.push("/teacher")}>
+            Lärare
+          </Button>
         </Stack>
       </Toolbar>
     </AppBar>
